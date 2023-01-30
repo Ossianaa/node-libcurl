11584ff2db794f538cd635574bd50cc22eabd8ff
ossianaa <857984597@qq.com>
2023-01-30 17:52:44 +0800

    sync the ja3 fp to linux version

--- test/libcurl_test.js
+++ test/libcurl_test.js
@@ -1,29 +1,34 @@
 const { LibCurl, fetch } = require('../index.js');
 
 async function main() {
-    /* const curl = new LibCurl();
-    curl.open('GET', 'http://127.0.0.1:51053/unittest/getJsonp')
-    curl.setProxy('127.0.0.1:10512')
+     const curl = new LibCurl();
+    curl.open('GET', 'https://tls.peet.ws/api/all')
     curl.setRequestHeader('user-Agent', 'chrome')
     await curl.send();
-    // console.log(curl.getResponseString());
-    console.log(curl.getResponseJsonp());
-    return
-    curl.open('GET', 'http://127.0.0.1:51053/unittest/getRawHeaders')
-    curl.setRequestHeader('user-Agent', 'chrome')
-    curl.removeCookie("a", "127.0.0.1")
-    await curl.send();
-    console.log(curl.getCookies());
     console.log(curl.getResponseString());
-    return */
-    fetch('https://juejin.cn/post/', {
-        cookies: 'a=b;c=d;e=f',
-        headers: {
-        }
-    }).then(e => e.json()).then(e => {
-        console.log(e);
-    }).catch((e) => {
-        console.log(e);
-    })
+
+    curl.open('GET', 'https://tls.peet.ws/api/all')
+    curl.setRequestHeader('user-Agent', 'chrome')
+    await curl.send();
+    console.log(curl.getResponseString());
+    return
+    // curl.open('GET', 'http://127.0.0.1:51053/unittest/getRawHeaders')
+    // curl.setRequestHeader('user-Agent', 'chrome')
+    // curl.removeCookie("a", "127.0.0.1")
+    // await curl.send();
+    // console.log(curl.getCookies());
+    // console.log(curl.getResponseString());
+    // return
+
+    // fetch('https://tls.peet.ws/api/clean', {
+    //     cookies: 'a=b;c=d;e=f',
+    //     headers: {
+    //         "user-Agent":"1"
+    //     }
+    // }).then(e => e.json()).then(e => {
+    //     console.log(e);
+    // }).catch((e) => {
+    //     console.log(e);
+    // })
 }
 main()
