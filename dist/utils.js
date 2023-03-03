06b88e4bb6058a92b65d6c537937a9b607e4e708
Ossian <857984597@qq.com>
2023-03-03 16:16:39 +0800

    ts update

--- dist/utils.js
+++ dist/utils.js
@@ -18,7 +18,7 @@ const httpCookiesToArray = (cookies) => {
             continue;
         }
         const [domain, secure, path, cors, timestamp, name, value] = it.split('\t');
-        cookies_.push([domain, stringBooleanToJsBoolean(secure), path, stringBooleanToJsBoolean(cors), parseInt(timestamp), name, value]);
+        cookies_.push([domain.replace(/#HttpOnly_/i, ''), stringBooleanToJsBoolean(secure), path, stringBooleanToJsBoolean(cors), parseInt(timestamp), name, value]);
     }
     return cookies_;
 };
