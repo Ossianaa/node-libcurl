68962e4c3d3ccae6e3b86e68d64e4be00c2d9e12
Ossian <857984597@qq.com>
2023-03-31 16:59:23 +0800

    fix ja3 fp bug

--- dist/utils.js
+++ dist/utils.js
@@ -84,13 +84,13 @@ const libcurlSetCookies = (curl, cookies, domain) => {
 };
 exports.libcurlSetCookies = libcurlSetCookies;
 const libcurlRandomJA3Fingerprint = () => {
-    const extensions = '0-23-65281-10-11-35-16-5-13-18-51-45-43-27-17513-21-41'.split('-');
+    const extensions = '0-23-65281-10-11-35-16-5-13-18-51-45-43-27-17513'.split('-');
     const randomBytes = node_crypto_1.default.getRandomValues(new Uint32Array(extensions.length));
     for (let i = randomBytes.length - 1; i >= 0; i--) {
         const pos = randomBytes[i] % (i + 1);
         [extensions[i], extensions[pos]] = [extensions[pos], extensions[i]];
     }
-    return `771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,${extensions.join('-')},29-23-24,0`;
+    return `771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,${extensions.join('-')}-21-41,29-23-24,0`;
 };
 exports.libcurlRandomJA3Fingerprint = libcurlRandomJA3Fingerprint;
 const md5 = (e) => {
