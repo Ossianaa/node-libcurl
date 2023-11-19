191dbf16475c9b258d297fb86283951a38d56bce
Ossian <857984597@qq.com>
2023-11-19 15:47:12 +0800

    feat: add local binding

--- scripts/bindings.js
+++ scripts/bindings.js
@@ -3,7 +3,12 @@ const assert = require('assert');
 const bindings = (() => {
     let binding;
     try {
-        binding = require('./postinstall');
+        binding = require('../build/Release/bao_curl_node_addon.node')
+    } catch (error) {
+        
+    }
+    try {
+        binding ||= require('./postinstall');
         const BaoLibCurl = binding.BaoLibCurl;
         assert.ok(BaoLibCurl, 'Failed to read target BaoLibCurl from native binary.');
         return binding;
