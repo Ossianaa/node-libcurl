44f7c4d5f12410f89f68fd4afd8e213f3be803a8
Ossian <857984597@qq.com>
2024-02-20 01:36:59 +0800

    ci: add mac arm64

---
+++ scripts/npm/bindings-arm64-apple-darwin/readme.md
@@ -0,0 +1,3 @@
+# `@ossiana/node-libcurl-arm64-apple-darwin`
+
+This is the **arm64-apple-darwin** binary for `@ossiana/node-libcurl`
--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,41 +1,37 @@
-const {
-    readFileSync
-} = require('fs');
+const { readFileSync } = require("fs");
 
-const {
-    platform,
-    arch
-} = process;
+const { platform, arch } = process;
 let nativeBinding = null;
 let loadError = null;
 
 function isMusl() {
     // For Node 10
-    if (!process.report || typeof process.report.getReport !== 'function') {
+    if (!process.report || typeof process.report.getReport !== "function") {
         try {
-            const lddPath = require('child_process').execSync('which ldd').toString().trim();
+            const lddPath = require("child_process")
+                .execSync("which ldd")
+                .toString()
+                .trim();
 
-            return readFileSync(lddPath, 'utf8').includes('musl');
+            return readFileSync(lddPath, "utf8").includes("musl");
         } catch (e) {
             return true;
         }
     } else {
-        const {
-            glibcVersionRuntime
-        } = process.report.getReport().header;
+        const { glibcVersionRuntime } = process.report.getReport().header;
         return !glibcVersionRuntime;
     }
 }
 
 switch (platform) {
-    case 'android':
+    case "android":
         throw new Error(`Unsupported architecture on Android ${arch}`);
 
-    case 'win32':
+    case "win32":
         switch (arch) {
-            case 'x64':
+            case "x64":
                 try {
-                    nativeBinding = require('@ossiana/node-libcurl-win32-x64-msvc');
+                    nativeBinding = require("@ossiana/node-libcurl-win32-x64-msvc");
                 } catch (e) {
                     loadError = e;
                 }
@@ -48,34 +44,43 @@ switch (platform) {
 
         break;
 
-    case 'darwin':
+    case "darwin":
         switch (arch) {
-            case 'x64':
+            case "x64":
                 try {
-                    nativeBinding = require('@ossiana/node-libcurl-darwin-x64');
+                    nativeBinding = require("@ossiana/node-libcurl-darwin-x64");
                 } catch (e) {
                     loadError = e;
                 }
 
                 break;
+            case "arm64":
+                try {
+                    nativeBinding = require("@ossiana/node-libcurl-darwin-arm64");
+                } catch (e) {
+                    loadError = e;
+                }
 
+                break;
             default:
                 throw new Error(`Unsupported architecture on macOS: ${arch}`);
         }
 
         break;
 
-    case 'freebsd':
+    case "freebsd":
         throw new Error(`Unsupported architecture on FreeBSD: ${arch}`);
 
-    case 'linux':
+    case "linux":
         switch (arch) {
-            case 'x64':
+            case "x64":
                 if (isMusl()) {
-                    throw new Error(`Unsupported architecture on Linux: ${arch} musl`);
+                    throw new Error(
+                        `Unsupported architecture on Linux: ${arch} musl`,
+                    );
                 } else {
                     try {
-                        nativeBinding = require('@ossiana/node-libcurl-linux-x64-gnu');
+                        nativeBinding = require("@ossiana/node-libcurl-linux-x64-gnu");
                     } catch (e) {
                         loadError = e;
                     }
