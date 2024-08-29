edb80bdbc98d2e85dac2e27e4aade4ee2f2c9635
Ossian <857984597@qq.com>
2024-08-29 16:30:38 +0800

    feat: Update ja3

--- src/utils.ts
+++ src/utils.ts
@@ -118,7 +118,7 @@ export const libcurlRandomJA3Fingerprint: () => string = () => {
         const pos = randomBytes[i] % (i + 1);
         [extensions[i], extensions[pos]] = [extensions[pos], extensions[i]];
     }
-    return `771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,${extensions.join("-")}-21-41,29-23-24,0`;
+    return `771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,${extensions.join("-")},25497-29-23-24,0`;
 };
 
 export const md5 = (e: string) => {
