cdc71dc14eda8431f132855cc993517e74237e49
Ossian <857984597@qq.com>
2024-10-19 14:54:06 +0800

    fix: remove `CURLOPT_PIPEWAIT` setting (#23)

--- src/libcurl/bao_curl.cpp
+++ src/libcurl/bao_curl.cpp
@@ -58,7 +58,6 @@ void BaoCurl::init()
 	CHECK_CURLOK(curl_easy_setopt(this->m_pCURL, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1));
 	CHECK_CURLOK(curl_easy_setopt(this->m_pCURL, CURLOPT_COOKIEFILE, NULL));
 	CHECK_CURLOK(curl_easy_setopt(this->m_pCURL, CURLOPT_PRIVATE, this));
-	CHECK_CURLOK(curl_easy_setopt(this->m_pCURL, CURLOPT_PIPEWAIT, 1L));
 	CHECK_CURLOK(curl_easy_setopt(this->m_pCURL, CURLOPT_FORBID_REUSE, 1L));
 	// setHttp2NextStreamId(1);
 	this->setTimeout(15, 15);
