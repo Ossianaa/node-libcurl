5fe1b10adc4f49bf26e5696c726dff203db390b4
Ossian <857984597@qq.com>
2026-08-14 04:26:58 +0800

    feat: Add randomization `initial_rtt` to transport_params

--- src/libcurl.ts
+++ src/libcurl.ts
@@ -297,11 +297,10 @@ const LibCurlHttp3FingerPrintImplMap: {
     chrome126: () => ({
         scid: "scid=0",
         settings: "1:65536;6:262144;7:100;51:1;GREASE",
-        transport_params:
-            `9:103;18258:1;3:1472;4:15728640;GREASE;1:30000;8:100;32:65536;15:AUTO;7:6291456;5:6291456;12583:${randomInt(
-                120000,
-                200000,
-            )};6:6291456;16741339:1@GREASE,1`,
+        transport_params: `9:103;18258:1;3:1472;4:15728640;GREASE;1:30000;8:100;32:65536;15:AUTO;7:6291456;5:6291456;12583:${randomInt(
+            120000,
+            200000,
+        )};6:6291456;16741339:1@GREASE,1`,
         tls: "ciphers=1,2,3;alps=h3;grease=off;rand=on",
         permutation: "7,0,14,19,15,9,4,24,17,21,1",
         verify_sigalgs:
